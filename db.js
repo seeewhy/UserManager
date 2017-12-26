ca5bff4ddfcf52b7d80e0a5de43685b3b5233b6a
Chnonso Obiora <chinonso.obiora@gmail.com>
2017-12-26 22:06:45 +0100

    DB_URL comment

--- db.js
+++ db.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+// @mongo is the name of the server we are connecting to within the container & not @localhost
 var DB_URL = 'mongodb://@mongo:27017/usermanager';
 
 mongoose.connect(DB_URL, { useMongoClient: true });
